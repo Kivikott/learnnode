a9a939f50be0872cc0b1e4f11362c2795740eeea
Kivikott <Mirell.Kivi@tptlive.ee>
2025-09-25 13:50:37 +0300

    add array

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-let text = `Hello `;
-text = text + `Mirell! `;
-console.log(text);
-text = `asdasd`;
-text = `fesisef`;
-text = "Dont use dobble quotes unless needed";
-text = `Don\'t use dobble quotes unless needed`
-console.log(text);
-let name = 'Mirell';
-let age = '17';
-text = 'your name is' + name + 'and you are ' + age + 'years old'
-console.log(text);
-text = `your name is ${name} and you are ${age} years old`
+let array = [1, 2, 3, 'asdasd', true, [1, 2, 3]]
+console.log(array);
+console.log(array[2]);
+console.log(array[5][1]);
+array [3] = 'hello';
+console.log(array);
+console.log(array.length);
+array.push('new value');
+console.log(array);
+let text = array.join('UwU');
 console.log(text);
