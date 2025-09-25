001ff20abd33431152d520164ea76038e515254a
Kivikott <Mirell.Kivi@tptlive.ee>
2025-09-25 13:36:39 +0300

    text

---
+++ index.js
@@ -0,0 +1,14 @@
+let text = `Hello `;
+text = text + `Mirell! `;
+console.log(text);
+text = `asdasd`;
+text = `fesisef`;
+text = "Dont use dobble quotes unless needed";
+text = `Don\'t use dobble quotes unless needed`
+console.log(text);
+let name = 'Mirell';
+let age = '17';
+text = 'your name is' + name + 'and you are ' + age + 'years old'
+console.log(text);
+text = `your name is ${name} and you are ${age} years old`
+console.log(text);
