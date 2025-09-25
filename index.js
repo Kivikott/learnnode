d4bbe001a48bd934e7974fab78723f430959d750
Kivikott <Mirell.Kivi@tptlive.ee>
2025-09-25 14:24:33 +0300

    functsion

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-let obj ={
-    name: 'Mirell'
-    age: 17,
-    cake:[vanilla, sokolate, egg],
-    cat:{
-        age:3,
-        name: 'egg',
-        gender: 'F'
-    },
-    parentName: 'Urmas',
-    a2sd: asd,
-    'parent-name': 'name'
-    '2asd': 'asd',
-};
+function hello() {
+    console.log ('Hello World!');
+}
 
-console.log(obj);
-console.log(obj.name);
-console.log(obj.cake[1]);
-console.log(obj.name);
-console.log(obj['parent-name']);
-console.log(Object.keys(obj));
