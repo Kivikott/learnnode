3715e3879bcbde8aafdfefab763ccddb0ea1d709
Kivikott <mirell.kivi@tptlive.ee>
2025-10-09 13:38:32 +0300

    add code they only works in browser

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-let text = 'Hello World';
-console.log(text[3]);
-let arrey = [1, 2, 3];
-let num = 2;
-num.toExponential();
-arrey.fill(1, 3,6);
-console.log(text.length);
-console.log(Math.PI);
+console.log('Hello World');
+console.log(document, window);
+ 
  
  
  
